12909b6a8373f57a3aa468a76938f1e25e7cdda4
ana.goettens <goettensana@gmail.com>
2023-08-01 17:57:14 -0300

    closes #8-arrayToString

--- src/nameArrayToString.ts
+++ src/nameArrayToString.ts
@@ -14,5 +14,17 @@
  * @return {string}
  */
 export const nameArrayToString = (nameArray: string[]) => {
-  return '';
+  const length = nameArray.length;
+
+  if (length === 0) {
+    return '';
+  } else if (length === 1) {
+    return nameArray[0];
+  } else if (length === 2) {
+    return nameArray.join(' & ');
+  } else {
+    const namesCopy = [...nameArray];
+    const lastElement = namesCopy.pop();
+    return namesCopy.join(', ') + ` & ${lastElement}`;
+  }
 };
